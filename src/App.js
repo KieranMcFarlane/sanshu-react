fbe28fe6c9514360d9d648d7f9e27224b97083df
Kieran Finder <kieranfinder@Kierans-MacBook-Pro.local>
2022-06-16 04:28:21 +0100

    Added custom fonts to tailwind theme. However I will still need to extend the default theme tomorrow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1 className="text-6xl">Do what I do</h1>
+      <h1 className="text-6xl">Sanshu is a <span className="text-purple-700">community</span></h1>
+      <p>I wanted the overall style to evoke travels, the adventures, and the personal connections that the Esperanto project represented. To do so, I knew I wanted a primary font with a humanist feel to it and a hand-drawn logo. 
+
+Style to evoke travels, the adventures, and the personal connections that the Esperanto project represented. I wanted a primary font with a humanist feel to it and a hand-drawn logo.</p>
     </>
   );
 }
