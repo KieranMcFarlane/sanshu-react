b2db0f0a5aa5333090ecdc1a92e3c2407092ba33
Kieran Finder <kieranfinder@Kierans-MacBook-Pro.local>
2022-06-16 20:18:39 +0100

    Extended font styles in tailwind via defaultTheme

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1 className="text-6xl">Sanshu is a <span className="text-purple-700">community</span></h1>
-      <p>I wanted the overall style to evoke travels, the adventures, and the personal connections that the Esperanto project represented. To do so, I knew I wanted a primary font with a humanist feel to it and a hand-drawn logo. 
+      <h1 className="text-6xl font-chain">Sanshu is a <span className="text-purple-700">community</span></h1>
+      <p className="font-aldrich">I wanted the overall style to evoke travels, the adventures, and the personal connections that the Esperanto project represented. To do so, I knew I wanted a primary font with a humanist feel to it and a hand-drawn logo. 
 
 Style to evoke travels, the adventures, and the personal connections that the Esperanto project represented. I wanted a primary font with a humanist feel to it and a hand-drawn logo.</p>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'chain': ["'chainwhacks'", ...defaultTheme.fontFamily.sans],
+        'aldrich': ["'aldrich-regular'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
