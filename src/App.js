1f0e771515314a52a13f66dad166220c0b496a2b
Kieran Finder <kieranfinder@Kierans-MacBook-Pro.local>
2022-06-16 02:28:24 +0100

    fixed create-react-app version issue in conjunction with tailwind-css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './index.css';
+import './App.css';
+import React from 'react'
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl">Do what I do</h1>
+    </>
+  );
+}
+
+export default App;
